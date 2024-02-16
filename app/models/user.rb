9cd9fd0635075e4925daec366e135503ce9d83f1
Creator912 <walker.joshua912@yahoo.com>
2024-02-16 10:22:55 -0800

    updated jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  devise :database_authenticatable, :registerable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+  
+          devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+          has_many :apartments               
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_15_230220) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_16_174204) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "apartments", force: :cascade do |t|
+    t.string "street"
+    t.string "unit"
+    t.string "city"
+    t.string "state"
+    t.integer "square_footage"
+    t.string "price"
+    t.integer "bedrooms"
+    t.float "bathrooms"
+    t.string "pets"
+    t.text "image"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
