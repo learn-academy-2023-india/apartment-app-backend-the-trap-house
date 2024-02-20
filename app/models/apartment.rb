30fbe6e9879c6eec51013bd0f631a7078720c59a
Sakaa4Short <klanyenek@gmail.com>
2024-02-20 11:34:31 -0800

    Removed API validations

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,5 +1,4 @@
 class Apartment < ApplicationRecord
     belongs_to :user
-    validates :street, presence: true
   end
 end
--- spec/models/apartment_spec.rb
+++ spec/models/apartment_spec.rb
@@ -1,25 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Apartment, type: :model do
-  let(:user) { User.create(
-    email: 'test@example.com',
-    password: 'password',
-    password_confirmation: 'password'
-    )
-  }
-
-  it 'should validate street' do
-    apartment = user.apartments.create(
-      unit: '2A',
-      city: 'Little Whinging',
-      state: 'Surrey',
-      square_footage: 2000,
-      price: '2000',
-      bedrooms: 3,
-      bathrooms: 2,
-      pets: 'yes',
-      image: 'https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg'
-    )
-    expect(apartment.errors[:street]).to include("can't be blank")
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
